b88f1ce6fbddbcb24b0ac23252b3f42a37d37a7e
rahilisashaik <rahilisashaik@berkeley.edu>
2024-12-25 22:01:05 -0500

    added heat map of air quality

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,11 @@ class Utils():
         return (measured - min_threshold) / (max_threshold - min_threshold)
     def ppm_to_mg_monitor(ppm, molar_mass):
         return ((ppm * molar_mass) / 24.45) * 0.00024150000000000002
+    
+    def parse_coordinates(coord):
+        lat, lon = coord.split(" N ")[0], coord.split(" N ")[1].replace(" W", "")
+        return float(lat), -float(lon)
+
 
 
 
