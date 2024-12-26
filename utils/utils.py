7aeae2c71313c65828527d4b63ed903f4ccbddb0
rahilisashaik <rahilisashaik@berkeley.edu>
2024-12-25 19:52:55 -0500

    initial commit with EDA, raw data, empirical data

---
+++ utils/utils.py
@@ -0,0 +1,9 @@
+class Utils():
+    def compute_normalized_value(measured, min_threshold, max_threshold):
+        return (measured - min_threshold) / (max_threshold - min_threshold)
+    def ppm_to_mg_monitor(ppm, molar_mass):
+        return ((ppm * molar_mass) / 24.45) * 0.00024150000000000002
+
+
+
+
